d59098dea4956d6b7b52196d27666956bdfcfa5d
Oleg Burov <oleg.burov@outlook.com>
2018-11-11 16:08:33 -0800

    Add unit tests.

---
+++ src/Docker/Task2/SinglyLinkedList.Tests/SinglyLinkedList_IsPalindromeShould.cs
@@ -0,0 +1,167 @@
+using Xunit;
+
+namespace SinglyLinkedList.Tests
+{
+  public class SinglyLinkedList_IsPalindromeShould
+  {
+    [Fact]
+    public void Return_True_Given_Sequence_Of_None()
+    {
+      // Arrange 
+      var list = new List.SinglyLinkedList();
+
+      // Act
+      var actual = list.IsPalindrome();
+
+      // Assert
+      Assert.True(actual);
+    }
+
+    [Fact]
+    public void Return_True_Given_Sequence_Of_1()
+    {
+      // Arrange 
+      var list = new List.SinglyLinkedList();
+      list.Add(1);
+
+      // Act
+      var actual = list.IsPalindrome();
+
+      // Assert
+      Assert.True(actual);
+    }
+
+    [Fact]
+    public void Return_True_Given_Sequence_Of_1_1()
+    {
+      // Arrange 
+      var list = new List.SinglyLinkedList();
+      list.Add(1);
+      list.Add(1);
+
+      // Act
+      var actual = list.IsPalindrome();
+
+      // Assert
+      Assert.True(actual);
+    }
+
+    [Fact]
+    public void Return_True_Given_Sequence_Of_1_2_1()
+    {
+      // Arrange 
+      var list = new List.SinglyLinkedList();
+      list.Add(1);
+      list.Add(2);
+      list.Add(1);
+
+      // Act
+      var actual = list.IsPalindrome();
+
+      // Assert
+      Assert.True(actual);
+    }
+
+    [Fact]
+    public void Return_True_Given_Sequence_Of_1_1_1()
+    {
+      // Arrange 
+      var list = new List.SinglyLinkedList();
+      list.Add(1);
+      list.Add(1);
+      list.Add(1);
+
+      // Act
+      var actual = list.IsPalindrome();
+
+      // Assert
+      Assert.True(actual);
+    }
+
+    [Fact]
+    public void Return_True_Given_Sequence_Of_2_5_5_2()
+    {
+      // Arrange 
+      var list = new List.SinglyLinkedList();
+      list.Add(2);
+      list.Add(5);
+      list.Add(5);
+      list.Add(2);
+
+      // Act
+      var actual = list.IsPalindrome();
+
+      // Assert
+      Assert.True(actual);
+    }
+
+    [Fact]
+    public void Return_False_Given_Sequence_Of_2_5()
+    {
+      // Arrange 
+      var list = new List.SinglyLinkedList();
+      list.Add(2);
+      list.Add(5);
+
+      // Act
+      var actual = list.IsPalindrome();
+
+      // Assert
+      Assert.False(actual);
+    }
+
+    [Fact]
+    public void Return_True_Given_Sequence_Of_2_1_3_1_2()
+    {
+      // Arrange 
+      var list = new List.SinglyLinkedList();
+      list.Add(2);
+      list.Add(1);
+      list.Add(3);
+      list.Add(1);
+      list.Add(2);
+
+      // Act
+      var actual = list.IsPalindrome();
+
+      // Assert
+      Assert.True(actual);
+    }
+
+    
+
+    [Fact]
+    public void Return_False_Given_Sequence_Of_2_5_5_1()
+    {
+      // Arrange 
+      var list = new List.SinglyLinkedList();
+      list.Add(2);
+      list.Add(5);
+      list.Add(5);
+      list.Add(1);
+
+      // Act
+      var actual = list.IsPalindrome();
+
+      // Assert
+      Assert.False(actual);
+    }
+
+    [Fact]
+    public void Return_False_Given_Sequence_Of_2_5_1_2()
+    {
+      // Arrange 
+      var list = new List.SinglyLinkedList();
+      list.Add(2);
+      list.Add(5);
+      list.Add(1);
+      list.Add(2);
+
+      // Act
+      var actual = list.IsPalindrome();
+
+      // Assert
+      Assert.False(actual);
+    }
+  }
+}
