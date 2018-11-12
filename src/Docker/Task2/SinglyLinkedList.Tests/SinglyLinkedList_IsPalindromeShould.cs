37d1bad97ea8d92516c17a1e88b81073fc244f34
Oleg Burov <oleg.burov@outlook.com>
2018-11-11 16:57:47 -0800

    Add test using words.

--- src/Docker/Task2/SinglyLinkedList.Tests/SinglyLinkedList_IsPalindromeShould.cs
+++ src/Docker/Task2/SinglyLinkedList.Tests/SinglyLinkedList_IsPalindromeShould.cs
@@ -6,7 +6,7 @@ namespace SinglyLinkedList.Tests
   public class SinglyLinkedList_IsPalindromeShould
   {
     [Fact]
-    public void Return_True_Given_Empty_Sequence()
+    public void Return_True_Given_Empty_Sequence_Of_Numbers()
     {
       // Arrange 
       var list = new SinglyLinkedList<int>();
@@ -23,7 +23,7 @@ namespace SinglyLinkedList.Tests
     [InlineData(new[] { 1, 1 })]
     [InlineData(new[] { 1, 1, 1 })]
     [InlineData(new[] { 1, 2, 1 })]
-    public void Return_True_Given_Trivial_Sequence(int[] sequence)
+    public void Return_True_Given_Trivial_Sequence_Of_Numbers(int[] sequence)
     {
       // Arrange 
       var list = new SinglyLinkedList<int>(sequence);
@@ -40,7 +40,7 @@ namespace SinglyLinkedList.Tests
     [InlineData(new[] { 2, 5, 0, 5, 2 })]
     [InlineData(new[] { 2, 5, 0, 0, 5, 2 })]
     [InlineData(new[] { 2, 5, 0, 1, 0, 5, 2 })]
-    public void Return_True_Given_Palindrome_Sequence(int[] sequence)
+    public void Return_True_Given_Palindrome_Sequence_Of_Numbers(int[] sequence)
     {
       // Arrange 
       var list = new SinglyLinkedList<int>(sequence);
@@ -59,10 +59,71 @@ namespace SinglyLinkedList.Tests
     [InlineData(new[] { 2, 5, 0, 2 })]
     [InlineData(new[] { 2, 5, 0, 5, 1 })]
     [InlineData(new[] { 2, 5, 0, 4, 2 })]
-    public void Return_False_Given_Not_Palindrome_Sequence(int[] sequence)
+    public void Return_False_Given_Not_Palindrome_Sequence_Of_Numbers(int[] sequence)
     {
       // Arrange 
       var list = new SinglyLinkedList<int>(sequence);
+
+      // Act
+      var actual = list.IsPalindrome();
+
+      // Assert
+      Assert.False(actual);
+    }
+
+    [Fact]
+    public void Return_True_Given_Empty_Sequence_Of_Characters()
+    {
+      // Arrange 
+      var list = new SinglyLinkedList<char>();
+
+      // Act
+      var actual = list.IsPalindrome();
+
+      // Assert
+      Assert.True(actual);
+    }
+
+    [Theory]
+    [InlineData(new[] { 'I' })]
+    [InlineData(new[] { 'd', 'a', 'd' })]
+    [InlineData(new[] { 'y', 'e', 'y' })]
+    public void Return_True_Given_Trivial_Sequence_Of_Characters(char[] sequence)
+    {
+      // Arrange 
+      var list = new SinglyLinkedList<char>(sequence);
+
+      // Act
+      var actual = list.IsPalindrome();
+
+      // Assert
+      Assert.True(actual);
+    }
+
+    [Theory]
+    [InlineData(new[] { 'r', 'o', 't', 'o', 'r' })]
+    [InlineData(new[] { 'l', 'e', 'v', 'e', 'l' })]
+    [InlineData(new[] { 'r', 'a', 'c', 'e', 'c', 'a', 'r' })]
+    public void Return_True_Given_Palindrome_Sequence_Of_Characters(char[] sequence)
+    {
+      // Arrange 
+      var list = new SinglyLinkedList<char>(sequence);
+
+      // Act
+      var actual = list.IsPalindrome();
+
+      // Assert
+      Assert.True(actual);
+    }
+
+    [Theory]
+    [InlineData(new[] { 'c', 'a', 't' })]
+    [InlineData(new[] { 'd', 'o', 'g' })]
+    [InlineData(new[] { 'a', 'n', 'd' })]
+    public void Return_False_Given_Not_Palindrome_Sequence_Of_Characters(char[] sequence)
+    {
+      // Arrange 
+      var list = new SinglyLinkedList<char>(sequence);
 
       // Act
       var actual = list.IsPalindrome();
