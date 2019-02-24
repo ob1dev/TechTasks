719f7e92a61279e071d9a5c722aa12baf22ce640
Oleg Burov <oleg.burov@outlook.com>
2019-02-23 22:21:12 -0800

    Add additional tests.

--- src/Microsoft/Task3/Graph.Tests/UndirectedGraph_FindMaximumWeightedPathShould.cs
+++ src/Microsoft/Task3/Graph.Tests/UndirectedGraph_FindMaximumWeightedPathShould.cs
@@ -146,10 +146,81 @@ namespace Graph.Tests
       graph.AddEdge(d, c, 4);
 
       // Act
-      var actual = graph.FindMaxWeightedPath(a, c);
+      var actual1 = graph.FindMaxWeightedPath(a, c);
+      var actual2 = graph.FindMaxWeightedPath(c, a);
 
       // Assert
-      Assert.Equal(7, actual);
+      Assert.Equal(7, actual1);
+      Assert.Equal(7, actual2);
+    }
+
+    [Fact]
+    public void Return_100_Given_Graph_With_4_Vertexes_5_Edges()
+    {
+      // Arrange 
+      var graph = new UndirectedGraph();
+
+      var a = new Vertex("A");
+      var b = new Vertex("B");
+      var c = new Vertex("C");
+      var d = new Vertex("D");
+
+      graph.AddVertex(a);
+      graph.AddVertex(b);
+      graph.AddVertex(c);
+      graph.AddVertex(d);
+
+      graph.AddEdge(a, b, 10);
+      graph.AddEdge(b, c, 20);
+      graph.AddEdge(a, d, 30);
+      graph.AddEdge(d, c, 40);
+      graph.AddEdge(b, d, 50);
+
+      // Act
+      var actual1 = graph.FindMaxWeightedPath(a, c);
+      var actual2 = graph.FindMaxWeightedPath(c, a);
+
+      // Assert
+      Assert.Equal(100, actual1);
+      Assert.Equal(100, actual2);
+    }
+
+    [Fact]
+    public void Return_70_Given_Graph_With_6_Vertexes_7_Edges()
+    {
+      // Arrange 
+      var graph = new UndirectedGraph();
+
+      var a = new Vertex("A");
+      var b = new Vertex("B");
+      var c = new Vertex("C");
+      var d = new Vertex("D");
+      var e = new Vertex("E");
+      var f = new Vertex("F");
+
+      graph.AddVertex(a);
+      graph.AddVertex(b);
+      graph.AddVertex(c);
+      graph.AddVertex(d);
+      graph.AddVertex(e);
+      graph.AddVertex(f);
+
+      graph.AddEdge(a, b, 10);
+      graph.AddEdge(b, c, 20);
+      graph.AddEdge(c, e, 40);
+      graph.AddEdge(e, f, 10);
+
+      graph.AddEdge(b, d, 30);
+      graph.AddEdge(d, e, 50);
+      graph.AddEdge(c, d, 60);
+
+      // Act
+      var actual1 = graph.FindMaxWeightedPath(a, f);
+      var actual2 = graph.FindMaxWeightedPath(f, a);
+
+      // Assert
+      Assert.Equal(150, actual1);
+      Assert.Equal(150, actual2);
     }
   }
 }
